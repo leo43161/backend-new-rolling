da9b9c46bdf8bf8109a098c57f8c1880e041fdc6
Leonardo Palavecino <leo43161301gmail.com>
2020-09-18 00:46:01 -0300

    Se realizo el metodo get para una noticia general

--- src/routes/news.routes.js
+++ src/routes/news.routes.js
@@ -3,11 +3,11 @@ const router = Router();
 import newsController from '../controllers/news.controller';
 import newsHighlightsController from '../controllers/newsHighlights.controller';
 
-const { getNews, postNew, deleteNew } = newsController;
+const { getNews, postNew, deleteNew, getNew } = newsController;
 const { postNewHighlight, getNewHighlight } = newsHighlightsController;
 
 router.route("/").get(getNews).post(postNew);
-router.route("/:id").delete(deleteNew).get()
+router.route("/:id").delete(deleteNew).get(getNew)
 router.route("/highlights").get(getNewHighlight).post(postNewHighlight);
 
 export default router;
