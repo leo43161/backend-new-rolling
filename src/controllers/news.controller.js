13f6124b40dcaa7e07627a48b28bf80ed8368f03
Leonardo Palavecino <leo43161301gmail.com>
2020-09-18 00:43:52 -0300

    Se realizo la funcion para poder editar una noticia en controllers

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -51,6 +51,15 @@ newsCtrl.deleteNew = async (req, res) => {
   }
 };
 
+newsCtrl.putNew = async (req, res) => {
+  try {
+    await New.findByIdAndUpdate(req.params.id, req.body)
+    res.status(200).json({mensaje: "La noticia de edito correctamente"})
+  } catch (error) {
+    res.status(500).json({mensaje: "Ocurrio un error al intentar editar la noticia"})
+  }
+}
+
 newsCtrl.getNew = async (req, res) => {
   try {
     const noticiaEncontrada = await New.findById(req.params.id);
