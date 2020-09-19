563c0608e990557118e0e136107d479271ce8292
Leonardo Palavecino <leo43161301gmail.com>
2020-09-19 02:24:05 -0300

    Se realizo una prueba de los metodos

--- src/routes/userAdm.routes.js
+++ src/routes/userAdm.routes.js
@@ -5,6 +5,6 @@ import userAdmController from '../controllers/userAdm.controller';
 const { getUserAdm, putUserAdm, postUserAdm } = userAdmController;
 
 router.route("/").get(getUserAdm).post(postUserAdm);
-router.route("/:id").post(postUserAdm);
+router.route("/:id").put(putUserAdm);
 
 export default router;
